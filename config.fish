11eeaa4902c5fe235becf5c3c4d84c895dd817b6
CyrilFerlicot <cyril@ferlicot.me>
2019-10-31 16:15:45 +0100

    add sudo alias

--- config.fish
+++ config.fish
@@ -16,6 +16,8 @@ alias .............="cd ../../../../../../../../../../../.."
 alias pcd="pushd"
 alias pd="popd"
 
+alias please="sudo"
+
 #See issue https://github.com/fish-shell/fish-shell/issues/1569
 
 function x86
