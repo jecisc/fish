ee1fc92971e4ca92ef82acdfa622f0f0e15ba85a
CyrilFerlicot <cyril@ferlicot.me>
2024-09-16 14:42:57 +0200

    Add alias pac for pullAndClean

--- config.fish
+++ config.fish
@@ -34,6 +34,11 @@ alias vachercher="git fetch --all"
 
 alias pl="/Applications/PharoLauncher.app/Contents/MacOS/pharo-launcher"
 
+alias verveineJ="/Users/cyril/Library/Preferences/pharo/GitRepositories/moosetechnology/VerveineJ/verveinej.sh"
+
+# This is an alias of my own command but I use it a lot so let's have a really short version on top of the long one
+alias pac="pullAndClean" 
+
 # See issue https://github.com/fish-shell/fish-shell/issues/1569
 
 function x86
